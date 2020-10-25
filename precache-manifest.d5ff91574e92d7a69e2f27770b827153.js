b93fc18e8f010d00140246f513f7bde874a5ded0
Christian Otu <christianotu.d@gmail.com>
2020-10-25 14:20:22 +0100

    Updates

---
+++ precache-manifest.d5ff91574e92d7a69e2f27770b827153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136b25c2b7c886469980a45fd74ea7d4",
+    "url": "/FEM-Easybank/index.html"
+  },
+  {
+    "revision": "e71a9ac7b3558f944e2f",
+    "url": "/FEM-Easybank/static/css/main.7e5a15d8.chunk.css"
+  },
+  {
+    "revision": "7d13f4b7ec31334116c9",
+    "url": "/FEM-Easybank/static/js/2.5492a136.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FEM-Easybank/static/js/2.5492a136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a9ac7b3558f944e2f",
+    "url": "/FEM-Easybank/static/js/main.760facb6.chunk.js"
+  },
+  {
+    "revision": "4813adeeab2616baa7be",
+    "url": "/FEM-Easybank/static/js/runtime-main.6f161db1.js"
+  }
+]);
